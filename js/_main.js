ff839de5e8b40647808bd9a6ddbab1639757792d
GovernorFancyPants <tobiasjohansson123@gmail.com>
2016-08-02 13:46:12 +0200

    Strava link on mobile

--- js/_main.js
+++ js/_main.js
@@ -1,37 +1,51 @@
-$(document).ready(function(){
-  smoothScroll.init({
-      speed: 500, // Integer. How fast to complete the scroll in milliseconds
-      easing: 'easeInOutCubic', // Easing pattern to use
-      updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
-      offset: 0 // Integer. How far to offset the scrolling anchor location in pixels
-  });
+$(document).ready(function() {
+    smoothScroll.init({
+        speed: 500, // Integer. How fast to complete the scroll in milliseconds
+        easing: 'easeInOutCubic', // Easing pattern to use
+        updateURL: false, // Boolean. Whether or not to update the URL with the anchor hash on scroll
+        offset: 0 // Integer. How far to offset the scrolling anchor location in pixels
+    });
 });
 
 $(document).ready(function() {
     var iframes = document.getElementsByTagName('iframe');
     for (var i = 0; i != iframes.length; ++i) {
-        if(iframes[i].src.substr(0,22) === 'https://www.strava.com') {
-            $( iframes[i] ).wrap( "<div class='strava-container'></div>" );
+        if (iframes[i].src.substr(0, 22) === 'https://www.strava.com') {
+            $(iframes[i]).wrap("<div class='strava-container'></div>");
+
+            var stravaLink = $('<a/>', {
+                class: "stravatest",
+                href: iframes[i].src.substr(0, 43),
+                text: " Strava Activity"
+            });
+
+            var fa = $('<i/>', {
+                class: 'fa fa-adjust'
+            });
+
+            stravaLink.prepend(fa);
+
+            $(iframes[i]).parent().append(stravaLink);
         }
     }
 });
 
 // Off Canvas Sliding
-$(document).ready(function(){
-  // Menu button click
-  $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e){
-    // $('#js-body').toggleClass('no-scroll');
-    $('#js-menu, #js-menu-screen').toggleClass('is-visible');
-    $('#js-menu-trigger').toggleClass('slide close');
-    // $('#masthead, #page-wrapper').toggleClass('slide');
-    e.preventDefault();
-  });
+$(document).ready(function() {
+    // Menu button click
+    $('#js-menu-trigger,#js-menu-screen').on('click touchstart', function(e) {
+        // $('#js-body').toggleClass('no-scroll');
+        $('#js-menu, #js-menu-screen').toggleClass('is-visible');
+        $('#js-menu-trigger').toggleClass('slide close');
+        // $('#masthead, #page-wrapper').toggleClass('slide');
+        e.preventDefault();
+    });
 });
 
 // FitVids
-$(document).ready(function(){
-	// Target your .container, .wrapper, .post, etc.
-	$("#main").fitVids();
+$(document).ready(function() {
+    // Target your .container, .wrapper, .post, etc.
+    $("#main").fitVids();
 });
 
 // Table of Contents title. Change text to localize
